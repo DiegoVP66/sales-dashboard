34ff6bfa75e433cfbcc262731855b2be2530c9d7
DiegoVP66 <dvpdev33@gmail.com>
2022-02-16 15:16:05 -0300

    Add Header component

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,23 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Teste</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
---
+++ front-web/src/components/header/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="main-header-container">
+      <h1 className="main-header-title">Ds Sales</h1>
+    </header>
+  );
+}
+
+export default Header;
